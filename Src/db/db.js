b94a59e511926af6a98ab9041fcd458c2027fea1
Gaurav Pathak <gaurav999raj@gmail.com>
2026-01-05 11:25:36 +0530

    connect mongo atals

---
+++ Src/db/db.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+  try {
+    const conneInstance = mongoose.connect(
+      `${process.env.MONGODB_URL} / ${DB_NAME}`
+    );
+    console.log(
+      `\n mongodb connected !! DB HOST:${(await conneInstance).connection.host}`
+    );
+  } catch (error) {
+    console.log("MONGODB connection error", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
---
+++ Src/index.js
@@ -0,0 +1,33 @@
+//require('dotenv').config({path:'./env'}) :- completely working but ill use import for consistancy
+import dotenv from "dotenv";
+import connectDB from "./db/db.js";
+
+dotenv.config({
+  path: "./env",
+});
+connectDB();
+
+/* Approch 1 :- this is the approach where all in main file which is not good like 
+we are using react we cannot put everything in app.js or main.js so well go to the 
+second approch where another file only here to connect the file.
+import express from "express"
+const app = express()
+
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+    app.on("error", (error)=>{
+     console.log("ERROR :", error)
+     throw error
+    })
+
+    app.listen(process.env.PORT,()=>{
+     console.log(`app is listning on port ${process.env.PORT}`)
+    })
+
+  } catch (error) {
+    console.log("Error:", error);
+    throw error
+  }
+})();
+*/
